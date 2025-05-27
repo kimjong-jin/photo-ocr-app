ddcf01d2f1c9a31babffe6fde2f8c2909832d9c2
kimjong-jin <kbisss@naver.com>
2025-05-28 01:35:16 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,17 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    define: {
+      // 직접 define 안 해도 import.meta.env로 접근 가능하지만 명시하고 싶다면 아래처럼 사용
+      'import.meta.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY),
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
