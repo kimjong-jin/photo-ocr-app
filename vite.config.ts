a69ba8745f1d88815f50a66758b58e994407c2f6
kimjong-jin <kbisss@naver.com>
2025-05-31 11:49:58 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
       'import.meta.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY),
