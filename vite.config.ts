62d3317a5a27856b9d5da5995bd05990e831f2ed
kimjong-jin <kbisss@naver.com>
2025-06-24 23:59:07 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig, loadEnv } from 'vite';
 import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
