1f98dad08b858a3db8a1248e179c5bfa33b1612b
kimjong-jin <kbisss@naver.com>
2025-08-18 00:31:45 +0900

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import path from "path";
-
-export default defineConfig({
-  resolve: { alias: { "@": path.resolve(__dirname, "src") } },
-  base: "./", // 배포 필수
-});
