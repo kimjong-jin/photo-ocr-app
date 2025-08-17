cad97c98cb956382def80a3a7237f5ef027a53ce
kimjong-jin <kbisss@naver.com>
2025-08-18 00:01:10 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
-// vite.config.ts (또는 .js)
-import { defineConfig, loadEnv } from 'vite';
+// vite.config.ts
+import { defineConfig } from 'vite';
 import path from 'path';
 
-export default defineConfig(({ mode }) => {
-  // mode: 'development' | 'preview' | 'production' 등
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    define: {
-      __API_KEY__: JSON.stringify(env.VITE_API_KEY ?? ''),
-      __LOAD_TEMP_API_URL__: JSON.stringify(env.VITE_LOAD_TEMP_API_URL ?? ''),
-      __SAVE_TEMP_API_URL__: JSON.stringify(env.VITE_SAVE_TEMP_API_URL ?? ''),
-    },
-    resolve: {
-      alias: { '@': path.resolve(__dirname, 'src') },
-    },
-    base: './',
-  };
+export default defineConfig({
+  resolve: {
+    alias: { '@': path.resolve(__dirname, 'src') },
+  },
+  base: './',
 });
