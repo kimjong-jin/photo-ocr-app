360b01a07f314e42b52b7db0da8d22399c30042f
kimjong-jin <kbisss@naver.com>
2025-08-17 23:37:06 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,19 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+  const env = loadEnv(mode, '.', '');
+  return {
+    define: {
+      'process.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY),
+      'process.env.VITE_LOAD_TEMP_API_URL': JSON.stringify(env.VITE_LOAD_TEMP_API_URL),
+      'process.env.VITE_SAVE_TEMP_API_URL': JSON.stringify(env.VITE_SAVE_TEMP_API_URL),
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+    base: './', // EXE 빌드 시 필수
+  };
 });
+
