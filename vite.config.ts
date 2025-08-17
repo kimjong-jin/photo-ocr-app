4dfd293b1742e23841689e188b10007f32ccd2e7
kimjong-jin <kbisss@naver.com>
2025-08-18 01:49:37 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname  = path.dirname(__filename);
+const __dirname  = path.dirname(__filename);
 
 export default defineConfig({
-  resolve: { alias: { "@": path.resolve(__dirname, "src") } },
-  base: "./",
+  resolve: { alias: { "@": path.resolve(__dirname, "src") } },
+  base: "./",
 });
