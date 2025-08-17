656c311beda58be4bf0103f3e505ad2cb6458d30
kimjong-jin <kbisss@naver.com>
2025-08-18 00:08:08 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  resolve: {
-    alias: { '@': path.resolve(__dirname, 'src') },
-  },
+  resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
   base: './',
+  server: {
+    hmr: { overlay: false }, // ✅ 에러 오버레이 끄기
+  },
 });
