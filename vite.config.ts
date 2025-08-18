7368ecd535f6ee88c461d55845b5000200fbf50d
kimjong-jin <kbisss@naver.com>
2025-08-18 22:53:10 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), ''); // .env.local 불러오기
+
+  return {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
     },
-  },
+    define: {
+      __API_KEY__: JSON.stringify(env.VITE_API_KEY),
+      __SAVE_URL__: JSON.stringify(env.VITE_SAVE_TEMP_API_URL),
+      __LOAD_URL__: JSON.stringify(env.VITE_LOAD_TEMP_API_URL),
+    },
+  };
 });
+
