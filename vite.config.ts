dbbfaa1e771b7dc0166de7d6605418e3d7ba39ce
kimjong-jin <kbisss@naver.com>
2025-09-11 00:23:03 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,19 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-  // mode에 맞는 환경변수 로드 (prefix: VITE_ 가 붙은 것만 클라이언트에 노출)
+  // mode에 맞는 환경변수 로드
+  // VITE_ 접두사가 붙은 환경변수만 클라이언트에서 접근 가능
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     define: {
-      // 따로 process.env 주입할 필요 없이,
-      // import.meta.env.VITE_XXX 로 접근하면 됩니다.
+      // process.env.* 따로 주입할 필요 없음
+      // import.meta.env.VITE_XXX 로 바로 접근 가능
     },
     resolve: {
       alias: {
+        // src 디렉토리를 @ 로 매핑
+        // 예: import AnalysisPage from '@/components/analysis/AnalysisPage'
         '@': path.resolve(__dirname, '.'),
       },
     },
