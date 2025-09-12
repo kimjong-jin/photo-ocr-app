4811f8904152090f96edf38cf423b3a433ca7e1c
kimjong-jin <kbisss@naver.com>
2025-09-12 10:33:19 +0900

    Update naver-reverse-geocode.ts

--- api/naver-reverse-geocode.ts
+++ api/naver-reverse-geocode.ts
@@ -54,9 +54,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       return res.status(200).json({ address });
     }
 
-    return res
-      .status(404)
-      .json({ error: "주소를 찾을 수 없습니다.", details: data });
+    return res.status(404).json({ error: "주소를 찾을 수 없습니다." });
   } catch (err: any) {
     console.error("Naver API proxy error:", err);
     return res.status(500).json({ error: "Internal Server Error" });
