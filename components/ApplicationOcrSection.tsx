525f2c8b76ffc734b3e23a6ee23363e682a14d27
kimjong-jin <kbisss@naver.com>
2025-11-11 08:37:24 +0900

    Remove unused 'use client' directive

--- components/ApplicationOcrSection.tsx
+++ components/ApplicationOcrSection.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React, { useState, useCallback, useEffect } from 'react';
 import { ActionButton } from './ActionButton';
 import { ImageInput, ImageInfo } from './ImageInput';
