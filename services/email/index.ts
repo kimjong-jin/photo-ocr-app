1c6f36942eecaaf919eb0711e4cc0e936ef917da
kimjong-jin <kbisss@naver.com>
2025-11-11 21:37:06 +0900

    Create index.ts

---
+++ services/email/index.ts
@@ -0,0 +1,7 @@
+// services/email/index.ts
+export * from './attachments';
+export * from './brevo';
+export * from './handler';
+export * from './templates';
+export * from './schema';
+export * from './errors';
