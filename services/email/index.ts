9ad6de63922a3f4aeb16b13e3d9e7047141368c8
kimjong-jin <kbisss@naver.com>
2025-11-11 22:01:59 +0900

    Delete services/email directory

--- services/email/index.ts
+++
@@ -1,7 +0,0 @@
-// services/email/index.ts
-export * from './attachments';
-export * from './brevo';
-export * from './handler';
-export * from './templates';
-export * from './schema';
-export * from './errors';
